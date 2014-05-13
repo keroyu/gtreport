7f157383e30d49fea53a6492357b772aff4d0848
Kero Yu <cyudev@gmail.com>
2014-05-13 18:37:16 +0800

    template changes

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -3,12 +3,17 @@
 <head>
 <meta charset="utf-8">
 <title>設計部工作進度報告</title>
+
 <link rel="stylesheet" href="/css/base.css">
 <link rel="stylesheet" href="/css/normalize.css">
+<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
 <script src="/js/jquery-1.10.2.min.js"></script>
 <script src="/js/jquery-ui-1.10.1.min.js"></script>
 <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
 <script src="/js/all.js"></script>
+
+
+
 </head>
 <body>
 <div class="wrapper">
