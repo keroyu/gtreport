93065ea52eff0b9c571892cb79f262db63272c35
Kero Yu <cyudev@gmail.com>
2014-07-15 18:51:35 +0800

    Add new member's name

--- app/views/report.blade.php
+++ app/views/report.blade.php
@@ -14,7 +14,7 @@
 	<span><span class="option">專案類型：</span><select name="type" id="type"><option value="W">網頁</option><option value="P">平面</option><option value="M">多媒體 </option></select> *</span>
 	<span><span class="option">本週進度：</span><select name="progress" id="progress" class="md"><option value="" selected>-</option><option value="已完成">已完成</option><option value="進行中">進行中</option><option value="其他">其他</option></select>*</span>
 	<span><span class="option">目前狀況：</span><input type="text" class="lg" name="status" id="status"></span>
-	<span><span class="option">負責人：</span><input type="text" class="lg" name="designer" id="designer">*<div class="name-box" id="nameBox"><ul><li>Sam</li><li>Mori</li><li>阿堡</li><li>姐姐</li><li>盧卡斯</li><li>Kero</li><li>虎牙</li><li>阿泰</li><li>小瑜</li><li>屁屁</li><li>Sai</li><li>阿港</li><li>阿男</li><li>Jimi</li></ul></div></span>
+	<span><span class="option">負責人：</span><input type="text" class="lg" name="designer" id="designer">*<div class="name-box" id="nameBox"><ul><li>Sam</li><li>Mori</li><li>阿堡</li><li>姐姐</li><li>Kero</li><li>虎牙</li><li>阿泰</li><li>小瑜</li><li>屁屁</li><li>Sai</li><li>阿港</li><li>阿男</li><li>Jimi</li><li>Claire</li></ul></div></span>
 	<span><span class="option">配合單位：</span><input type="text" class="md" name="cowork" id="cowork"></span>
 	<span><span class="option">連結網址：</span><input type="text" class="lg" name="url" id="url"></span>
 	<div class="text-center"><button class="btn primary sm" id="addNewTask">確定送出</button></div>
@@ -23,7 +23,7 @@
 	<div class="project" id="tasksTable">
 	</div>
 	<input type="hidden" id="reportSN" value="<?php echo $sn; ?>" >
-	<script src="/js/taskManager.min.js"></script>
+	<script src="../../js/taskManager.min.js"></script>
 
 @if ( $mode == 'edit' )
 <div class="text-center margin-tb"><a class="btn info lg"  href="/report/<?php echo $sn ?>/print" target="_blank">查看列印版</a></div>
