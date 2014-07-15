88ab49730eae42b93724bce99a04cd1b456356fa
Kero Yu <cyudev@gmail.com>
2014-07-15 18:51:05 +0800

    Remove unnecessary call

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -5,13 +5,13 @@
 <title>設計部工作進度報告</title>
 
 <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
-<script src="/js/jquery-1.10.2.min.js"></script>
-<script src="/js/jquery-ui-1.10.1.min.js"></script>
+<script src="http://gt.dev/js/jquery-1.10.2.min.js"></script>
+<script src="http://gt.dev/js/jquery-ui-1.10.1.min.js"></script>
 <link rel="stylesheet" href="//code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
 <!-- featured by Kero Yu -->
-<link rel="stylesheet" href="/css/base.css">
-<link rel="stylesheet" href="/css/normalize.css">
-<script src="/js/all.min.js"></script>
+<link rel="stylesheet" href="http://gt.dev/css/base.css">
+<link rel="stylesheet" href="http://gt.dev/css/normalize.css">
+<script src="http://gt.dev/js/all.min.js"></script>
 </head>
 <body>
 <div class="wrapper">
